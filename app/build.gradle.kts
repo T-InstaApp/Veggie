3acc9a87a5868bb9152ed1e47b5b7d6967a47072
Niraj <Niraj.kumar@tissatech.com>
2024-03-21 23:24:52 +0530

    Update Gradle file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+    id("com.google.firebase.crashlytics")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -47,6 +50,10 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+    buildFeatures {
+        viewBinding = true
+        dataBinding = true
+    }
 }
 
 dependencies {
@@ -66,4 +73,42 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+
+    // Image loading
+    implementation("com.github.bumptech.glide:glide:4.13.2")
+    //PrivateSharedPreferences
+    implementation("androidx.preference:preference-ktx:1.2.1")
+    //Text Size
+    implementation("com.intuit.sdp:sdp-android:1.0.6")
+    //ViewModel and Live Data
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
+    //Retrofit and gson
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    //Kotlin Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
+    //kodein Dependency Injection
+    implementation("org.kodein.di:kodein-di-generic-jvm:6.2.1")
+    implementation("org.kodein.di:kodein-di-framework-android-x:6.2.1")
+    //paging
+    implementation("androidx.paging:paging-runtime:3.2.1")
+
+    //Google Place
+    implementation("com.google.android.libraries.places:places:3.3.0")
+    //Google Map
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
+
+    //Payment
+    implementation("com.razorpay:checkout:1.6.33")
+
+    //Firebase
+    implementation(platform("com.google.firebase:firebase-bom:30.3.1"))
+    implementation("com.google.firebase:firebase-crashlytics-ktx")
+    implementation("com.google.firebase:firebase-analytics-ktx")
+
+
 }
