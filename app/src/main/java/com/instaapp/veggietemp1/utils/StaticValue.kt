1a4db64a3f86225423c6c0713f964bbe314a1df3
T-InstaApp <144309913+T-InstaApp@users.noreply.github.com>
2024-04-04 18:03:01 +0530

    Update via Python script

--- app/src/main/java/com/instaapp/veggietemp1/utils/StaticValue.kt
+++ app/src/main/java/com/instaapp/veggietemp1/utils/StaticValue.kt
@@ -5,7 +5,7 @@ object StaticValue {
     //private const val PRODUCTION_BASE_URL = "https://restaurant60-be-prod-xtpocjmkpa-uw.a.run.app/"
     private const val DEVELOPMENT_BASE_URL = "https://restaurant60-be-dev-xtpocjmkpa-uw.a.run.app/"
     const val BASE_URL = DEVELOPMENT_BASE_URL
-    const val REST_ID = "912"//492,225
+    const val REST_ID = "916"//492,225
     const val TEMP_RESTRO_ID = "1"
     const val REST_USER_NAME = "admin"
     const val REST_PASSWORD = "Mphasis8"
